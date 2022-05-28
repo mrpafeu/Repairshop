a9382c4962c704cdb779e215490c86e646335e3e
mrpafeu <62951197+mrpafeu@users.noreply.github.com>
2022-05-28 16:11:31 +0200

    Add files via upload

---
+++ src/test/java/com/wsiiz/repairshop/enterprise/application/EmployeeControllerTest.java
@@ -0,0 +1,24 @@
+package com.wsiiz.repairshop.enterprise.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+    @Test
+    void getMany() {
+    }
+
+    @Test
+    void getOne() {
+    }
+
+    @Test
+    void addNew() {
+    }
+
+    @Test
+    void remove() {
+    }
+}
